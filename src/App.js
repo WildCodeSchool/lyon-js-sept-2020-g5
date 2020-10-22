61935ff2edf1d4153cb03cdaaadb39515492a14f
cleroy117 <ce.leroy69@gmail.com>
2020-10-22 11:32:13 +0200

    Mise en place des routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
+import Game from './Components/Game';
+import Rules from './Components/Rules';
+import Options from './Components/Options';
+import AboutUs from './Components/AboutUs';
+import ContactUs from './Components/ContactUs';
 
 function App() {
-  return <div></div>;
+  return <div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/game">Start new game</Link>
+            </li>
+            <li>
+              <Link to="/rules">Rules</Link>
+            </li>
+            <li>
+              <Link to="/options">Options</Link>
+            </li>
+            <li>
+              <Link to="/aboutUs">About us</Link>
+            </li>
+            <li>
+              <Link to="/contactUs">Contact us</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/game" component={Game} />
+          <Route path="/rules" component={Rules} />
+          <Route path="/options" component={Options} />
+          <Route path="/aboutUs" component={AboutUs} />
+          <Route path="/contactUs" component={ContactUs} />  
+        </Switch>
+      </div>
+    </Router>
+  </div>;
 }
 
 export default App;
