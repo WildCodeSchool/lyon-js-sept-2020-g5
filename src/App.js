fc2b5d46478e608297f68d8836c34961caf224c7
cleroy117 <ce.leroy69@gmail.com>
2020-10-27 15:05:49 +0100

    set up eslint and prettier

--- src/App.js
+++ src/App.js
@@ -9,47 +9,48 @@ import AboutUs from './Components/AboutUs';
 import ContactUs from './Components/ContactUs';
 
 function App() {
-  return <div>
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/game">Start new game</Link>
-            </li>
-            <li>
-              <Link to="/rules">Rules</Link>
-            </li>
-            <li>
-              <Link to="/options">Options</Link>
-            </li>
-            <li>
-              <Link to="/aboutUs">About us</Link>
-            </li>
-            <li>
-              <Link to="/contactUs">Contact us</Link>
-            </li>
-          </ul>
-        </nav>
+  return (
+    <div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/game">Start new game</Link>
+              </li>
+              <li>
+                <Link to="/rules">Rules</Link>
+              </li>
+              <li>
+                <Link to="/options">Options</Link>
+              </li>
+              <li>
+                <Link to="/aboutUs">About us</Link>
+              </li>
+              <li>
+                <Link to="/contactUs">Contact us</Link>
+              </li>
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/game" component={Game} />
-          <Route path="/rules" component={Rules} />
-          <Route path="/options" component={Options} />
-          <Route path="/aboutUs" component={AboutUs} />
-          <Route path="/contactUs" component={ContactUs} />  
-        </Switch>
-      </div>
-    </Router>
-  </div>;
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/game" component={Game} />
+            <Route path="/rules" component={Rules} />
+            <Route path="/options" component={Options} />
+            <Route path="/aboutUs" component={AboutUs} />
+            <Route path="/contactUs" component={ContactUs} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
-
 export default App;
--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-
 const ContactUs = () => {
-    return (
-        <div>
-            <h1>ContactUs</h1>
-        </div>
-    );
-}
- 
+  return (
+    <div>
+      <h1>ContactUs</h1>
+    </div>
+  );
+};
+
 export default ContactUs;
