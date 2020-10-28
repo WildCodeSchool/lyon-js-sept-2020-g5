e573ba7ba2dd2e856028594f3b3c18ab9796c60e
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2020-10-28 15:42:10 +0100

    responsive design is done for mobile, tablet, desktop

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,20 +7,30 @@ const Home = (props) => {
   const handleNewGameClick = () => {
     props.history.push("/game")
   }
+
+  const handleRulesClick = () => {
+    props.history.push("/rules")
+  }
+
+  const handleOptionsClick = () => {
+    props.history.push("/options")
+  }
+
+  const handleAboutClick = () => {
+    props.history.push("/aboutUs")
+  }
   return (
     <div className="body-home-container">
-    <div className="box">
 
-    </div>
-    <div className="btn-container1">
-        <button className="btn-home" className="btn-new-game" onClick={handleNewGameClick}>New Game</button>
+      <div className="btn-container1">
+          <button className="btn-home" className="btn-new-game" onClick={handleNewGameClick}>New Game</button>
 
-    </div>
-    <div className="btn-container2">
-        <button className="btn-home" className="btn-rules">Rules</button>
-        <button className="btn-home" className="btn-options">Options</button>
-        <button className="btn-home" className="btn-about">About us</button>
-    </div>
+      </div>
+      <div className="btn-container2">
+          <button className="btn-home" className="btn-rules" onClick={handleRulesClick}>Rules</button>
+          <button className="btn-home" className="btn-options" onClick={handleOptionsClick}>Options</button>
+          <button className="btn-home" className="btn-about" onClick={handleAboutClick}>About us</button>
+      </div>
 
     </div>
     
