704ea771b586044fc145c0aca34cc3ea437a65f3
cleroy117 <ce.leroy69@gmail.com>
2020-10-28 14:43:03 +0100

    basic header and footer created

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Style/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright 2020</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <p>Logo</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/options">Options</Link>
+          </li>
+          <li>
+            <Link to="/rules">Rules</Link>
+          </li>
+          <li>
+            <Link to="/hallofheroes">Hall of heroes</Link>
+          </li>
+          <li>
+            <Link to="/aboutUs">About us</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
