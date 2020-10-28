1b991bf1e6c60fdd6428a91395c3832e66515449
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2020-10-28 13:51:57 +0100

    there is background image and the buttons are designed

--- src/App.js
+++ src/App.js
@@ -13,35 +13,9 @@ function App() {
     <div>
       <Router>
         <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/game">Start new game</Link>
-              </li>
-              <li>
-                <Link to="/rules">Rules</Link>
-              </li>
-              <li>
-                <Link to="/options">Options</Link>
-              </li>
-              <li>
-                <Link to="/aboutUs">About us</Link>
-              </li>
-              <li>
-                <Link to="/contactUs">Contact us</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+          
           <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
+            <Route exact path="/" component={Home} />
             <Route path="/game" component={Game} />
             <Route path="/rules" component={Rules} />
             <Route path="/options" component={Options} />
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import '../style/Home.css'
 
-const Home = () => {
+const Home = (props) => {
+  console.log(props)
+  
+  const handleNewGameClick = () => {
+    props.history.push("/game")
+  }
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="body-home-container">
+    <div className="box">
+
     </div>
+    <div className="btn-container1">
+        <button className="btn-home" className="btn-new-game" onClick={handleNewGameClick}>New Game</button>
+
+    </div>
+    <div className="btn-container2">
+        <button className="btn-home" className="btn-rules">Rules</button>
+        <button className="btn-home" className="btn-options">Options</button>
+        <button className="btn-home" className="btn-about">About us</button>
+    </div>
+
+    </div>
+    
   );
 };
 
