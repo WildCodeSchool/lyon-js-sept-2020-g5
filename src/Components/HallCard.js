87572aee30e4f3809d9a31446676fd78338b6ca1
eddymandran <dev.eddy.mandran@gmail.com>
2020-10-29 09:39:50 +0100

    modification de la requete de l'api pour utiliser uniquement les heros Marvel + 1ere mise en forme du CSS

--- src/Components/HallCard.js
+++ src/Components/HallCard.js
@@ -2,21 +2,44 @@ import React from 'react';
 import '../Style/hallCard.css';
 
 function HallCard(props) {
-  const { name, img, atk, hp, power, universe, alignment } = props;
+  const { name, img, atk, hp, power, alignment, id } = props;
   return (
     <div className="HallCard">
-      <h1>
-        Name:
-        {name}
-      </h1>
-      <img src={img} alt={name} />
-      <ul>
-        <li>Attack :{atk}</li>
-        <li>Power :{power}</li>
-        <li>HP :{hp}</li>
-        <li>Universe :{universe}</li>
-        <li>Alignment :{alignment}</li>
-      </ul>
+      <div>
+        <table>
+          <tr>
+            <td>Name: </td>
+            <td>{name}</td>
+          </tr>
+          <tr>
+            <td>ID: </td>
+            <td>{id}</td>
+          </tr>
+        </table>
+      </div>
+      <div>
+        <img src={img} alt={name} />
+      </div>
+      <div>
+        <table>
+          <tr>
+            <td>Attack :</td>
+            <td>{atk}</td>
+          </tr>
+          <tr>
+            <td>Power :</td>
+            <td>{power}</td>
+          </tr>
+          <tr>
+            <td>HP :</td>
+            <td>{hp}</td>
+          </tr>
+          <tr>
+            <td>Alignment :</td>
+            <td>{alignment}</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
