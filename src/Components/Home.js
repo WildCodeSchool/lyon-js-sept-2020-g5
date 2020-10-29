58c74a6eba86d96b913cf38b772bd951479542fd
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2020-10-29 11:24:14 +0100

    added library Howler, and audio on every button of the page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,27 +1,52 @@
 import React from 'react';
-import '../style/Home.css'
+import '../style/Home.css';
+import Punch from '../Audio/Punch.wav';
+import Go from '../Audio/go.wav';
+import {Howl, Howler} from "howler";
+
+const audioClips = new Howl ({
+  src:[Punch]
+  
+});
+const audioClips2 = new Howl ({
+  src:[Go]
+  
+});
 
 const Home = (props) => {
   console.log(props)
   
-  const handleNewGameClick = () => {
+
+  
+  
+
+const handleNewGameClick = () => {
     props.history.push("/game")
+      audioClips2.play()
+  
+    
+    
   }
 
-  const handleRulesClick = () => {
+const handleRulesClick = () => {
     props.history.push("/rules")
+    audioClips.play()
   }
 
   const handleOptionsClick = () => {
     props.history.push("/options")
+    audioClips.play()
   }
 
   const handleAboutClick = () => {
     props.history.push("/aboutUs")
+    audioClips.play()
   }
   return (
-    <div className="body-home-container">
 
+  
+    <div className="body-home-container">
+      
       <div className="btn-container1">
           <button className="btn-home" className="btn-new-game" onClick={handleNewGameClick}>New Game</button>
 
