20adb1ad805dc59364373d8dd4844e6135a8ed8c
eddymandran <dev.eddy.mandran@gmail.com>
2020-10-30 11:44:53 +0100

    suppression div inutiles

--- src/Components/HallCard.js
+++ src/Components/HallCard.js
@@ -4,37 +4,33 @@ import '../Style/hallCard.css';
 function HallCard(props) {
   const { name, img, atk, hp, power, alignment } = props;
   return (
-    <div>
-      <div className="cards-list">
-        <div className="card">
-          <table>
-            <thead>
-              <tr>
-                <th>{name}</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td rowSpan="5">
-                  <img src={img} alt={name} />
-                </td>
-              </tr>
-              <tr>
-                <td>Attack : {atk}</td>
-              </tr>
-              <tr>
-                <td>Power : {power}</td>
-              </tr>
-              <tr>
-                <td>HP : {hp}</td>
-              </tr>
-              <tr>
-                <td>Alignment : {alignment}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+    <div className="card">
+      <table>
+        <thead>
+          <tr>
+            <th>{name}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td rowSpan="5">
+              <img src={img} alt={name} />
+            </td>
+          </tr>
+          <tr>
+            <td>Attack : {atk}</td>
+          </tr>
+          <tr>
+            <td>Power : {power}</td>
+          </tr>
+          <tr>
+            <td>HP : {hp}</td>
+          </tr>
+          <tr>
+            <td>Alignment : {alignment}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
