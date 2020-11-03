31fb771806f1c4bdf4c545459a92fa21fdca3f31
eddymandran <dev.eddy.mandran@gmail.com>
2020-11-03 23:35:41 +0100

    mise en forme partiel de card et cardlist

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import '../Style/hallCard.css';
+import '../Style/Card.css';
+import sword from '../Pictures/icons-epee.png';
+import heart from '../Pictures/icons-coeurs.png';
+import hammer from '../Pictures/icons-marteau-de-thor.png';
 
 function Card(props) {
-  const { name, img, atk, hp, power, alignment } = props;
+  const { name, img, atk, hp, power } = props;
   return (
     <div className="card">
+      <div className="heroName">{name}</div>
       <div className="imgCard">
         <img src={img} alt={name} />
       </div>
-      <div className="stats">
-        <h1>{name}</h1>
-        <br />
-        <p>
-          Attack : <span className="statsInfo">{atk}</span>
-          <br />
-          Power : <span className="statsInfo">{power}</span>
-          <br />
-          HP : <span className="statsInfo">{hp}</span>
-          <br />
-          Alignment : <span className="statsInfo">{alignment}</span>
-        </p>
+      <div className="attack">
+        <img src={sword} alt="sword icons" /> {atk}
+      </div>
+      <div className="healthPoints">
+        <img src={heart} alt="heart icons" /> {hp}
+      </div>
+      <div className="power">
+        <img src={hammer} alt="hammer icons" /> {power}
       </div>
     </div>
   );
