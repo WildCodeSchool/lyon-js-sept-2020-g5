8972a603d0e1891809afe93f092f7a38f2bcf13b
cleroy117 <ce.leroy69@gmail.com>
2020-11-03 18:23:32 +0100

    orientation portait is not allowed for Smartphone

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -3,18 +3,15 @@ import '../Style/Game.css';
 
 const Game = () => {
   return (
-    <div>
+    <div className="pageGame">
       <div className="Portrait">
-        <h1>Portrait</h1>
         <img
           src="https://karagezwebstudio.com/fr/img/rotate.gif"
           alt="turn phone"
         />
       </div>
 
-      <div className="Paysage">
-        <h1>Paysage</h1>
-      </div>
+      <div className="Paysage"></div>
     </div>
   );
 };
