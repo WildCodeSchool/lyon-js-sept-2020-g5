936e6088868c7fba767245db5895662ce0c4d7f1
Rémi Cusset <56927808+lerem-s38@users.noreply.github.com>
2020-11-03 16:51:56 +0100

    mise en place gift chrgement

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import './../Style/Game.css';
+import '../Style/Game.css';
 
 const Game = () => {
   return (
     <div>
       <div className="Portrait">
-        <h1>Paysage</h1>
+        <h1>Portrait</h1>
+        <img
+          src="https://karagezwebstudio.com/fr/img/rotate.gif"
+          alt="turn phone"
+        />
       </div>
 
       <div className="Paysage">
-        <h1>Portrait</h1>
+        <h1>Paysage</h1>
       </div>
     </div>
   );
