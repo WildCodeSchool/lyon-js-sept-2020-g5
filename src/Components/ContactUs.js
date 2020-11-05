a3493518bf6cb05265ae24c5e12920094f398510
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2020-11-05 15:58:42 +0100

    contact page design is done

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import '../style/ContactUs.css'
+import '../Style/ContactUs.css';
 
 const ContactUs = () => {
   return (
-
     <div className="form-container">
-    <form className="form">
-      <h1 className="contact-title">Contact form </h1>
+      <form className="form">
+        <h1 className="contact-title">Contact form </h1>
 
-      
-      <input className="contact-input"  placeholder="name" />
+        <input className="contact-input" placeholder="name" />
 
-      <input className="contact-input" placeholder="email" />
+        <input className="contact-input" placeholder="email" />
 
-      <textarea className="contact-textarea" placeholder="Message" />
+        <textarea className="contact-textarea" placeholder="Message" />
 
-      <button className="btn-contact" type="submit">Send your message</button>
-
-
-    </form>
-
+        <button className="btn-contact" type="submit">
+          Send your message
+        </button>
+      </form>
     </div>
-      
   );
 };
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,70 +1,62 @@
 import React from 'react';
-import '../style/Home.css';
+import '../Style/Home.css';
 import Punch from '../Audio/Punch.wav';
 import Go from '../Audio/go.wav';
-import {Howl, Howler} from "howler";
+import { Howl, Howler } from 'howler';
 
-
-
-const audioClips = new Howl ({
-  src:[Punch]
-  
+const audioClips = new Howl({
+  src: [Punch],
 });
 
-const audioClips2 = new Howl ({
-  src:[Go]
-  
+const audioClips2 = new Howl({
+  src: [Go],
 });
 
 const Home = (props) => {
-    console.log(props)
-  
+  console.log(props);
 
-  
-  
+  const handleNewGameClick = () => {
+    props.history.push('/game');
+    audioClips2.play();
+  };
 
-const handleNewGameClick = () => {
-    props.history.push("/game")
-    audioClips2.play()
-  
-    
-    
-  }
-
-const handleRulesClick = () => {
-    props.history.push("/rules")
-    audioClips.play()
-  }
+  const handleRulesClick = () => {
+    props.history.push('/rules');
+    audioClips.play();
+  };
 
   const handleOptionsClick = () => {
-    props.history.push("/options")
-    audioClips.play()
-  }
+    props.history.push('/options');
+    audioClips.play();
+  };
 
   const handleAboutClick = () => {
-    props.history.push("/aboutUs")
-    audioClips.play()
-  }
-  
+    props.history.push('/aboutUs');
+    audioClips.play();
+  };
+
   return (
-
     <div className="body-home-container ">
-    <div className="btn-home-container">
-      
-      <div className="btn-container1">
-          <button  className="btn-new-game " onClick={handleNewGameClick}>New Game</button>
+      <div className="btn-home-container">
+        <div className="btn-container1">
+          <button className="btn-new-game " onClick={handleNewGameClick}>
+            New Game
+          </button>
+        </div>
 
+        <div className="btn-container2">
+          <button className="btn-rules" onClick={handleRulesClick}>
+            Rules
+          </button>
+          <button className="btn-options" onClick={handleOptionsClick}>
+            Options
+          </button>
+          <button className="btn-about" onClick={handleAboutClick}>
+            About us
+          </button>
+        </div>
       </div>
-
-      <div className="btn-container2">
-          <button  className="btn-rules" onClick={handleRulesClick}>Rules</button>
-          <button  className="btn-options" onClick={handleOptionsClick}>Options</button>
-          <button  className="btn-about" onClick={handleAboutClick}>About us</button>
-      </div>
-
     </div>
-    </div>
-    
   );
 };
 
