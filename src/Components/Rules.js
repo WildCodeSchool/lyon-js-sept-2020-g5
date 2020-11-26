4698036277811995d9489d2baab34cabf658efb2
gclement28 <clementgarcin38270@gmail.com>
2020-11-26 16:54:35 +0100

    rules photos are new

--- src/Components/Rules.js
+++ src/Components/Rules.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import '../Style/Rules.css';
-import boardgame1 from '../Pictures/boardgame1.png';
-import playerboard from '../Pictures/playerboard.png';
+import selectioncartes from '../Pictures/selectioncartes.png'
+import powermax from '../Pictures/powermax.png'
+import checkteamwindow from '../Pictures/checkteamwindow.png';
+import fightphase from '../Pictures/fightphase.png';
+import graveyardfull from '../Pictures/graveyardfull.png';
+import iaturnfight from '../Pictures/iaturnfight.png';
+import yourturnfight from '../Pictures/yourturnfight.png';
+import win from '../Pictures/win.png';
+
 
 import RulesComponents from './RulesComponents';
 
@@ -9,31 +16,31 @@ function Rules() {
   return (
     <div className="RulesContainer">
       <RulesComponents
-        image={boardgame1}
+        image={selectioncartes}
         alt="Deck choice"
         nameOfRule="Deck choice"
         textRule="Click on the card to add the hero to your deck."
       />
       <RulesComponents
-        image={playerboard}
+        image={powermax}
         alt="Deck choice - Max Power"
         nameOfRule="Deck choice - Max Power"
         textRule="Filled up to the maximum power allowed for your deck."
       />
       <RulesComponents
-        image={playerboard}
+        image={checkteamwindow}
         alt="Your game board - Hand"
         nameOfRule="Your game board - Hand"
         textRule="On your game board you can see the cards you can use for your fight."
       />
       <RulesComponents
-        image={playerboard}
+        image={yourturnfight}
         alt="Your game board - Your turn"
         nameOfRule="Your game board - Your turn"
         textRule="Among your visible cards, select ('click') a card to send it to the battle board."
       />
       <RulesComponents
-        image={playerboard}
+        image={yourturnfight}
         alt="Your game board - End of your turn"
         nameOfRule="Your game board - End of your turn"
         textRule="It's time to attack! 
@@ -41,27 +48,27 @@ function Rules() {
         Click or press `End-turn` to finish your turn."
       />
       <RulesComponents
-        image={playerboard}
+        image={iaturnfight}
         alt="Board opponent - turn"
         nameOfRule="Board opponent - turn"
         textRule="It's your opponent's turn!
         He will attack you with a hero from his deck if he doesn't already have one on his board."
       />
       <RulesComponents
-        image={playerboard}
+        image={fightphase}
         alt="Board, Fight phase"
         nameOfRule="Board, Fight phase"
         textRule="The cards are going to clash. The hit points decrease following the attacks. The red number corresponds to the attack points and the green one to the health points."
       />
       <RulesComponents
-        image={playerboard}
+        image={graveyardfull}
         alt="Graveyard"
         nameOfRule="Graveyard"
         textRule="If the hero is not powerful enough or too weak after these multiple fights, he dies and goes to the graveyard ! 
         We love them 3,000....."
       />
       <RulesComponents
-        image={playerboard}
+        image={win}
         alt="How to win"
         nameOfRule="How to win"
         textRule="You still have a hero still alive in your hand and or on your fighting board, while your opponent has none left. 
